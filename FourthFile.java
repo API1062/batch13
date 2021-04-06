7d59eaf07c0b154650b5f3c0a71676eda4be7756
Amanpreet <amanpreet.singh@apisero.com>
2021-04-06 10:52:59 +0530

    First Commit

---
+++ FourthFile.java
@@ -0,0 +1,7 @@
+class D
+{
+   public static void main(String args[])
+    {
+       System.out.println("This is class D)
+      }
+}
---
+++ ThirdFile.java
@@ -0,0 +1,7 @@
+public class C
+{
+   public static class void main(String args[])
+   {
+     System.out.println("This is class C");
+     }
+}
